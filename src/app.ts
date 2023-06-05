8c189bd55835d69e856981fc7746a567de877784
Daniela Tocua <danidatope@gmail.com>
2023-06-04 19:47:59 -0500

    more general

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,7 @@ import express, { Application } from "express";
 import helmet from "helmet";
 
 import { appDataSource } from "./dataSource";
-import animalRoutes from "./routes/animal.routes";
-import personRoutes from "./routes/person.routes";
+
 export class App {
 	private readonly _app: Application;
 
@@ -23,8 +22,7 @@ export class App {
 		this._app.use(helmet());
 		this._app.use(express.json());
 		this._app.use(express.urlencoded({ extended: true }));
-		this._app.use("/animal", animalRoutes);
-		this._app.use("/person", personRoutes);
+		//this._app.use("/animal", animalRoutes);
 	}
 
 	public get app(): Application {
